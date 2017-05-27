3f69234729ae5701107750c30613223ef1194e08
yangqi8930 <89304594@qq.com>
2017-05-27 16:59:58 +0800

    修复布局跳转bug

--- app/src/main/java/com/example/lee/myapplication/ApartmentItem.java
+++ app/src/main/java/com/example/lee/myapplication/ApartmentItem.java
@@ -24,7 +24,7 @@ public class ApartmentItem extends AppCompatActivity {
     }
     private void initApartment() {
         for (int i = 0; i <101; i++) {
-            Apartment runan = new Apartment(R.drawable.touxiang,"runan","工程师","Radio1", "runan.cai@huanuo-nokia.com", "18573197193", "sh");
+            Apartment runan = new Apartment(R.drawable.touxiang,"蔡如男","工程师","Radio1", "runan.cai@huanuo-nokia.com", "18573197193", "sh");
             apartmentList.add(runan);
         }
     }
