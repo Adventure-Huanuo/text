a81d1f22285d4f1e293b7a5ed19c3fc5ae9c17f3
shenzhonghan <zhonghan.shen@nokia.com>
2017-06-01 19:11:50 +0800

    差旅申请的界面

---
+++ app/src/main/java/com/example/lee/myapplication/travelapply.java
@@ -0,0 +1,33 @@
+package com.example.lee.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.TextUtils;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class travelapply extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.travelapply);
+
+    }
+
+
+
+    private void initApartment() {
+
+
+    }
+}
