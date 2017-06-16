5494eb9cb398defa6515ef40acf3e8658677d66e
cairunan <runan.cai@huanuo-nokia.com>
2017-06-16 15:50:39 +0800

    差旅布局更新

--- app/src/main/java/com/example/lee/myapplication/travelapply.java
+++ app/src/main/java/com/example/lee/myapplication/travelapply.java
@@ -1,42 +1,107 @@
 package com.example.lee.myapplication;
 
-import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.TextUtils;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class travelapply extends AppCompatActivity {
-    private static final String[] m={"HomeTrip","海外差旅","商务差旅","项目差旅"};
+
+    private static final String[] m = {"HomeTrip", "海外差旅", "商务差旅", "项目差旅"};
     private Spinner spinner;
     private ArrayAdapter<String> adapter;
+    private RadioGroup radioGroup;
+    private LinearLayout mPaper;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.travelapply);
-        spinner =(Spinner) findViewById(R.id.spinner_traveltype);
+        spinner = (Spinner) findViewById(R.id.spinner_traveltype);
         //将可选内容与ArrayAdapter连接起来
-        adapter =new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,m);
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, m);
         //设置下拉列表风格
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //将adapter添加到spinner中
         spinner.setAdapter(adapter);
         //设置默认值
         spinner.setVisibility(View.VISIBLE);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        mPaper = (LinearLayout) findViewById(R.id.info);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup arg0, int arg1) {
+                // TODO Auto-generated method stub
+                //获取变更后的选中项的ID
+                int radioButtonId = arg0.getCheckedRadioButtonId();
+                //根据ID获取RadioButton的实例
+                RadioButton rb = (RadioButton) travelapply.this.findViewById(radioButtonId);
+                //更新文本内容，以符合选中项
+                if (rb.getText().equals("是")) {
+                    mPaper.setVisibility(mPaper.VISIBLE);
+                } else {
+                    mPaper.setVisibility(mPaper.GONE);
+                }
+            }
+        });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     private void initApartment() {
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("travelapply Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
