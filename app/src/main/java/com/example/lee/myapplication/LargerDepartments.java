f1dbb1d0f456378fcced6bb1fc3d34689800b4ac
Escapist <15669909922@139.com>
2017-08-06 20:54:47 +0800

    自动获取大部门信息

---
+++ app/src/main/java/com/example/lee/myapplication/LargerDepartments.java
@@ -0,0 +1,12 @@
+package com.example.lee.myapplication;
+
+public class LargerDepartments {
+    private String item_name1;
+    public LargerDepartments(String item_name)
+    {
+        this.item_name1 = item_name;
+    }
+    public String getItem_name() {
+        return item_name1;
+    }
+}
